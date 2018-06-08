77f4ee73e750574ac4204fe437f601e7a97a93ce
Chidimma Uba <chidimmajuba@gmail.com>
2018-06-08 13:21:35 -0400

    Shazam version of FizzBuzz

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,23 +1,36 @@
 package com.company;
+
 import java.util.*;
+
 public class Main {
 
     public static void main(String[] args) {
-       System.out.println( workingVersion());
+        System.out.println(workingVersion());
     }
+
     private static String workingVersion() {
-        Random rnd = new Random();
         Integer x;
         String output = "";
-        for (x = 1; x <= 100; x++) {
-            if (x % 3 == 0 && x % 5 == 0) {
-                output+="FizzBuzz\n";
-            } else if (x % 5 == 0) {
-                output+="Buzz\n";
-            } else if (x % 3 == 0) {
-                output+="Fizz\n";
-            } else output+=x.toString() + "\n";
-            //toString converts integer to string
-        }return output;
+        Random rnd = new Random();
+        Integer max;
+        Integer min;
+        max = rnd.nextInt(100) + 1;
+        min = rnd.nextInt(100) + 1;
+        Integer i;
+        for (i = min; i <= max; i++) {
+                if (max == min) {
+                    return "Shazam";
+                }
+                if (i % 3 == 0 && i % 5 == 0) {
+                    output += "FizzBuzz\n";
+                } else if (i % 5 == 0) {
+                    output += "Buzz\n";
+                } else if (i % 3 == 0) {
+                    output += "Fizz\n";
+                } else output += i.toString() + "\n";
+                //toString converts integer to string
+            }return output;
+        }
     }
-}
+
+
