8cc4fe859daa4eaf882807449925954719669aea
Chidimma Uba <chidimmajuba@gmail.com>
2018-06-08 14:24:53 -0400

    This won't take long version of FizzBuzz.

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -17,20 +17,28 @@ public class Main {
         max = rnd.nextInt(100) + 1;
         min = rnd.nextInt(100) + 1;
         Integer i;
-        for (i = min; i <= max; i++) {
-                if (max == min) {
-                    return "Shazam";
-                }
-                if (i % 3 == 0 && i % 5 == 0) {
-                    output += "FizzBuzz\n";
-                } else if (i % 5 == 0) {
-                    output += "Buzz\n";
-                } else if (i % 3 == 0) {
-                    output += "Fizz\n";
-                } else output += i.toString() + "\n";
-                //toString converts integer to string
-            }return output;
+        if (max == min) {
+            return "Shazam";
         }
+        if (min > max) {
+            return "Oops";
+        }
+        if (max - min < 10) {
+            output = "This won't take long\n";
+        }
+        for (i = min; i <= max; i++) {
+            if (i % 3 == 0 && i % 5 == 0) {
+                output += "FizzBuzz\n";
+            } else if (i % 5 == 0) {
+                output += "Buzz\n";
+            } else if (i % 3 == 0) {
+                output += "Fizz\n";
+            } else output += i.toString() + "\n";
+            //toString converts integer to string
+        }
+        return output;
     }
+}
+
 
 
